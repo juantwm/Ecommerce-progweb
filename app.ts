611d9878171ea5bf4f82cfcfdbdb6e726e9c715f
Juanma <juanmatorres161@gmail.com>
2026-08-23 16:37:17 -0300

    estructura del proyecto

---
+++ app.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+
+
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) =>
+{
+    res.render("pages/index");
+
+});
+
+app.get("/products", (req, res)=>
+{
+    res.render("pages/product");
+});
+
+app.get("/cart", (req, res) =>
+{
+    res.render("pages/cart");
+
+});
+
+app.get("/login", (req,res) =>
+{
+    res.render("pages/login");
+});
+app.get("/register", (req,res) =>
+{
+    res.render("pages/register");
+});
+app.get("/checkout", (req,res) =>
+{
+    res.render("pages/checkout");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>
+{
+    console.log("Server is ready!");
+
+});
