572356fa0fcf5864fa33380d5c041a509e656c4c
Juanma <juanmatorres161@gmail.com>
2026-09-07 16:34:04 -0300

    faltan imagenes y acomodar los banners promocionales

--- app.ts
+++ app.ts
@@ -5,12 +5,32 @@ const app = express();
 
 const port = 3000;
 
+const categorias =[
+    {id:1, nombre:'Electronica', icono: 'bi-laptop'},
+    {id:2, nombre:'Alimentos', icono:'bi-basket'},
+    {id:3, nombre:'Bebidas', icono:'bi-cup-straw'},
+    {id:4, nombre:'Indumentaria', icono:'bi-bag'},
+    {id:5, nombre:'Juegos', icono:'bi-controller'},
+    {id:6, nombre:'Automotor', icono:'bi-car-front'},
+    {id:7, nombre:'Hogar', icono:'bi-house'},
+    {id:8, nombre:'Otros', icono:'bi-box'}
+];
+
+const listadoProductos = [
+        { nombre: 'Whiskey Jack Daniels 750ml', precio: '19.900' },
+        { nombre: 'Coca Cola Lata 220mL Pack x8', precio: '7.600' },
+        { nombre: 'Monitor 24 Pulgadas', precio: '185.000' },
+        { nombre: 'Silla Gamer', precio: '210.000' }
+];
+
+
 app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) =>
 {
-    res.render("pages/index");
+    res.render("pages/index", 
+        {mostrarBuscador: true, categorias: categorias, products: listadoProductos});
 
 });
 
